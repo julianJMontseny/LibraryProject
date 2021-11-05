665525dc361901fbf8600c5d1b0794cd4fda5fa6
DavidGonS <david.gonzalez.sanjose.8@gmail.com>
2021-11-05 09:21:46 +0100

    Insert Member implementado

--- library-backend/src/main/java/org/iesfm/library/dao/MemberDAO.java
+++ library-backend/src/main/java/org/iesfm/library/dao/MemberDAO.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface MemberDAO {
     List<Member> list();
+    void insert(Member member);
 }
--- library-backend/src/main/java/org/iesfm/library/dao/jdbc/JDBCMemberDAO.java
+++ library-backend/src/main/java/org/iesfm/library/dao/jdbc/JDBCMemberDAO.java
@@ -4,9 +4,21 @@ import org.iesfm.library.Member;
 import org.iesfm.library.dao.MemberDAO;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class JDBCMemberDAO implements MemberDAO {
+
+    private final static String INSERT_MEMBER =
+            "INSERT INTO member(" +
+                    "nif," +
+                    "name," +
+                    "surname)" +
+                    "VALUES(:nif," +
+                    ":name," +
+                    ":surname)";
+
     private NamedParameterJdbcTemplate jdbc;
 
     public JDBCMemberDAO(NamedParameterJdbcTemplate jdbc) {
@@ -18,5 +30,14 @@ public class JDBCMemberDAO implements MemberDAO {
     @Override
     public List<Member> list() {
         return null;
+    }
+
+    @Override
+    public void insert(Member member) {
+        Map<String,Object> memberParams = new HashMap<>();
+        memberParams.put("nif",member.getNif());
+        memberParams.put("name",member.getName());
+        memberParams.put("surname",member.getSurname());
+        jdbc.update(INSERT_MEMBER,memberParams);
     }
 }
