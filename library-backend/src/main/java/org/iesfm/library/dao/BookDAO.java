440867aa05704109d53137bc3a21d54469ffd491
CarMB <char.sakonia@gmail.com>
2021-11-05 08:46:43 +0100

    update method     public List<Book> bookListByYear();

--- library-backend/src/main/java/org/iesfm/library/dao/BookDAO.java
+++ library-backend/src/main/java/org/iesfm/library/dao/BookDAO.java
@@ -1,4 +1,15 @@
 package org.iesfm.library.dao;
 
+import org.iesfm.library.Book;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import java.util.List;
+
 public interface BookDAO {
+
+    public List<Book> bookListByYear();
+    
+
+
+
 }
