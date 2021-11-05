64efcfb78f9742a72c80952401388987afdb37db
AlejandroGomez <pipiton.pipitona@gmail.com>
2021-11-05 10:01:08 +0100

    correcion de errores

--- library-backend/src/main/java/org/iesfm/library/dao/BookDAO.java
+++ library-backend/src/main/java/org/iesfm/library/dao/BookDAO.java
@@ -1,25 +1,19 @@
 package org.iesfm.library.dao;
-
 import org.iesfm.library.Book;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import java.util.List;
 
 public interface BookDAO {
 
-
     List<Book> bookListByYear(int year);
 
     void bookModify();
 
-<<<<<<< HEAD
-    void insert (Book book);
+    void insert(Book book);
 
-    List<Book> listAll();
-}
-=======
     List<Book> listAll();
 
     List<Book> lookForBookWithAuthor(String author);
-    }
->>>>>>> b9c871164b6800e1eb1a5a79a19e279d9f797780
+}
+
+
