8c445ab94c6e4a0e60a84e7f7d9e871ba8aa0d8e
migsamu <mufrikiler@gmail.com>
2021-11-05 10:06:44 +0100

    finalizado metodo modificar libro

--- library-backend/src/main/java/org/iesfm/library/dao/BookDAO.java
+++ library-backend/src/main/java/org/iesfm/library/dao/BookDAO.java
@@ -1,7 +1,6 @@
 package org.iesfm.library.dao;
 
 import org.iesfm.library.Book;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import java.util.List;
 
@@ -12,14 +11,10 @@ public interface BookDAO {
 
     void bookModify(Book book);
 
-<<<<<<< HEAD
-    void insert (Book book);
+    void insert(Book book);
 
-    List<Book> listAll();
-}
-=======
     List<Book> listAll();
 
     List<Book> lookForBookWithAuthor(String author);
-    }
->>>>>>> b9c871164b6800e1eb1a5a79a19e279d9f797780
+
+}
